ec3b392301db78531e01e4977da8766b71deb930
Duncan Kipkemoi <80266796+Dan1820@users.noreply.github.com>
2023-05-25 00:42:52 +0300

    Update anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def matching_word(self, words):
+        return [word for word in words if sorted(self.word) == sorted(word)]
+
+
+listen = Anagram('listen')
+print(listen.matching_word(['enlist', 'google', 'inlets', 'banana']))
